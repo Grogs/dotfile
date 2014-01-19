70abfe1a2e5de91c4cfa3e38acf9c9878ab3c9e6
Greg Dorrell <greg@dorrell.me>
2014-01-19 14:21:05 +0000

    emacs: turn on line wrap, and add power line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,7 +46,8 @@
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
 
-(custom-set-variables
+
+
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
