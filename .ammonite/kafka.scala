dac87ca164ab4ea76508a891a4e951f056b179e4
Greg Dorrell <Greg@Dorrell.me>
2016-02-02 18:28:21 +0000

    Add helper for consuming from local kafka instance

--- .ammonite/kafka.scala
+++ .ammonite/kafka.scala
@@ -6,10 +6,14 @@ object kafka {
   import scala.collection.JavaConverters._
   import org.apache.kafka
   import org.apache.kafka.clients.producer.KafkaProducer
+  import org.apache.kafka.clients.consumer.KafkaConsumer
   import org.apache.kafka.common.serialization.StringSerializer
+  import org.apache.kafka.common.serialization.StringDeserializer
   import org.apache.kafka.clients.producer.ProducerRecord
 
   def producer(servers: String = "localhost:9092") = new KafkaProducer[String,String](Map[String,AnyRef]("bootstrap.servers"->servers).asJava, new StringSerializer, new StringSerializer)
+
+  def consumer(servers: String = "localhost:9092") = new KafkaConsumer[String,String](Map[String,AnyRef]("bootstrap.servers"->servers).asJava, new StringDeserializer, new StringDeserializer)
 
   implicit def tupleToProducerRecord[V](t: (String,V)) = new ProducerRecord[String,V](t._1, t._2)
 }
