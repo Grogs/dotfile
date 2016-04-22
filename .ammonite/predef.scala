9d30706245d124fc2b5a7f4282e58e5c0ad5685a
Greg Dorrell <Greg@Dorrell.me>
2016-04-22 17:02:07 +0100

    ammonite changes

--- .ammonite/predef.scala
+++ .ammonite/predef.scala
@@ -2,7 +2,9 @@ load.ivy("com.lihaoyi" %% "ammonite-shell" % ammonite.Constants.version)
 @
 val sess = ammonite.shell.ShellSession()
 import sess._
+import ammonite.shell.PPrints._
 import ammonite.ops._
+import ammonite.shell._
 ammonite.shell.Configure(repl, wd)
 
 
--- .ammonite/spark.scala
+++ .ammonite/spark.scala
@@ -4,10 +4,20 @@ import org.apache.spark.SparkConf
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-val appName = "Ammonite REPL instance"
-val master = "local[2]"
-val conf = new SparkConf().setAppName(appName).setMaster(master)
-val sc = new SparkContext(conf)
-
 Logger.getLogger("org.apache").setLevel(Level.WARN)
+
+load.ivy( "org.apache.spark" % "spark-core_2.11" % "1.6.0")
+load.ivy( "org.apache.spark" % "spark-sql_2.11" % "1.6.0")
+
+@
+
+object spark {
+  private val appName = "Ammonite REPL instance"
+  private val master = "local[2]"
+  val configuration = new SparkConf().setAppName(appName).setMaster(master)
+  val context = new SparkContext(configuration)
+  //val sqlContext = new org.apache.spark.sql.SQLContext(context)
+}
+
+spark //just to force object initialisation
 
